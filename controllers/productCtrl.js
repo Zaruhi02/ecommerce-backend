a236be888b6aeeeffb3467836964cde02069239a
Zaruhi02 <hanyanzaruhi@gmail.com>
2022-12-14 21:06:40 +0400

    first commit

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -5,27 +5,42 @@ const Products = require('../models/productModel')
 // Filter, sorting and paginating
 
 class APIfeatures {
-    constructor(query, queryString){
+    constructor(query, queryString) {
         this.query = query;
         this.queryString = queryString;
     }
     filtering() {
-        const queryObj = {...this.queryString}
-        console.log({before: queryObj})
+        const queryObj = { ...this.queryString }
         const excludedFields = ['page', 'sort', 'limit']
-        excludedFields.forEach(el => delete(queryObj[el]))
-
-        console.log({after: queryObj})
+        excludedFields.forEach(el => delete (queryObj[el]))
 
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
-        console.log({queryObj, queryStr});
-       
+
+
+        this.query.find(JSON.parse(queryStr))
+
         return this;
 
     }
-    sorting() {}
-    paginating() {}
+    sorting() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        } else {
+            this.query = this.query.sort('-createdAt')
+        }
+
+
+        return this;
+    }
+    paginating() { 
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
 }
 
 
@@ -33,10 +48,15 @@ const productCtrl = {
     getProducts: async (req, res) => {
         try {
             console.log(req.query)
-            const features = new APIfeatures(Products.find(), req.query).filtering()
+            const features = new APIfeatures(Products.find(), req.query)
+            .filtering().sorting().paginating()
             const products = await features.query
 
-            res.json(products)
+            res.json({
+                status: 'success',
+                result: products.length,
+                products: products
+            })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
@@ -76,7 +96,7 @@ const productCtrl = {
                 title: title.toLowerCase(), price, description, content, images, category
             })
 
-            res.json({msg: "Updated a Product"})
+            res.json({ msg: "Updated a Product" })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
